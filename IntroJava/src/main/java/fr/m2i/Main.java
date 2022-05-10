812f42ec5fc76538e2a375bb95f5429cbb5dcf89
Victor <victor.blangy@gmail.com>
2022-05-10 15:39:32 +0200

    initial commit

---
+++ IntroJava/src/main/java/fr/m2i/Main.java
@@ -0,0 +1,10 @@
+package fr.m2i;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
