6c887c582ce4e3925a48fae2023b8f196e17021c
Victor <victor.blangy@gmail.com>
2022-05-11 16:50:39 +0200

    corrigé de l'exercice 2

--- IntroJava/src/main/java/fr/m2i/Main.java
+++ IntroJava/src/main/java/fr/m2i/Main.java
@@ -1,18 +1,28 @@
 package fr.m2i;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Integer number = null;
 
-        try {
-            System.out.print("Saisissez un nombre : ");
-            int n = scanner.nextInt();
-            for (int i = 1; i <= 10;i++) {
-                System.out.println(n + " * " + i + " = " + n * i);
+        while (number == null) {
+            try {
+                System.out.print("Entrez un nombre : ");
+                number = scanner.nextInt(); // Exception potentiellement levée, donc la variable number reste à null
+                scanner.close();
+            } catch (InputMismatchException e) {
+                System.out.println("Attention ! Vous devez entrez un nombre");
+                scanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("Mince une erreur s'est produite :(");
+                return;
             }
-        } catch (Exception e){
-            System.out.println("Veuillez réessayer avec un nombre, si l'erreur persiste veuillez abandonner.");
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(String.format("%d x %d = %d", number, i, number * i));
         }
     }
 }
