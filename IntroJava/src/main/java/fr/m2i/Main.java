fc206be872c387934a3dc79b532303603ecc9a5b
Victor <victor.blangy@gmail.com>
2022-05-13 12:17:23 +0200

    exercice 4 partie 1

--- IntroJava/src/main/java/fr/m2i/Main.java
+++ IntroJava/src/main/java/fr/m2i/Main.java
@@ -3,13 +3,3 @@ package fr.m2i;
 public class Main {
 
     public static void main(String[] args) {
-
-        Calculator myCalculator = new Calculator();
-
-        try {
-            myCalculator.start();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-}
--- IntroJava/src/main/java/fr/m2i/Vehicle.java
+++ IntroJava/src/main/java/fr/m2i/Vehicle.java
@@ -1,14 +1,42 @@
 package fr.m2i;
 
-public class Vehicle {
+public abstract class Vehicle {
 
-    private int speed;
+    protected float currentGas;
+    protected float gasCapacity;
+    protected float gasConsumption;
+    protected int speed;
+    protected int passengerCapacity;
 
-    void accelerate() {
-
+    public float getCurrentGas() {
+        return currentGas;
+    }
+    public void setCurrentGas(float currentGas) {
+        this.currentGas = currentGas;
+    }
+    public float getGasCapacity() {
+        return gasCapacity;
+    }
+    public void setGasCapacity(float gasCapacity) {
+        this.gasCapacity = gasCapacity;
+    }
+    public float getGasConsumption() {
+        return gasConsumption;
+    }
+    public void setGasConsumption(float gasConsumption) {
+        this.gasConsumption = gasConsumption;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
     }
 
-    protected void fixEngine() {
-
-    }
 }
