88caa960d47491731bf808af1e4e3f41cfd82df0
Victor <victor.blangy@gmail.com>
2022-05-16 14:02:51 +0200

    ajout de main

--- IntroJava/src/main/java/fr/m2i/Main.java
+++ IntroJava/src/main/java/fr/m2i/Main.java
@@ -1,6 +1,13 @@
-package fr.m2i;
+package fr.m2i;git 
+
 public class Main {
+
     public static void main(String[] args) {
 
+        Soldier matteo = new Soldier("Mettéo", 100, 78, 45);
+        Wizard cecile = new Wizard("Cécile", 89, 95, 68);
+
+        cecile.move();
+        cecile.attack(matteo);
     }
 }
